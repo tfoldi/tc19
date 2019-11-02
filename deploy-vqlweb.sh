d95b830bda923021516f7303baf67eb32d9219cf
Földi Tamás <tfoldi@xsi.hu>
2019-11-02 08:42:39 -0400

    rename folders

--- deploy-vqlweb.sh
+++ deploy-vqlweb.sh
@@ -1,1 +1,1 @@
-cp -f extension/vqlweb.js "/mnt/c/Program Files/Tableau/Tableau Server/packages/vizqlserver.20194.19.0923.1135/public/v_201941909231135/javascripts/vqlweb.js"
+cp -f user-validator-extension/vqlweb.js "/mnt/c/Program Files/Tableau/Tableau Server/packages/vizqlserver.20194.19.0923.1135/public/v_201941909231135/javascripts/vqlweb.js"
