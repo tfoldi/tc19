91f14517f1d586c08aa18a0db20d4597e20ee1ad
Földi Tamás <tfoldi@xsi.hu>
2019-12-02 10:58:28 -0500

    remove debug log

--- hotjar/tableau-extensions-hotjar-client.js
+++ hotjar/tableau-extensions-hotjar-client.js
@@ -2,7 +2,6 @@
     const HOTJAR_REQUEST_MESSAGE = "Hotjar-Request";
 
     tableau.extensions.dashboardContent.dashboard.__proto__.addHotjarTracking = function (settings) {
-        console.log("Sync request from client");
 
         window.parent.postMessage(
             {
